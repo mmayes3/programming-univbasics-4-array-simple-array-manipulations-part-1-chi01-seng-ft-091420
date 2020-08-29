f31cd5126efcf3242d1e9cfc5cc20cf4eed60992
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-29 03:56:49 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,35 @@
+def using_push(array, string)
+  updated_array = array.push(string)
+end
+
+def using_unshift(array, string)
+  updated_array = array.unshift(string)
+end
+
+def using_pop(array)
+  k = array.pop
+  return k   
+end
+
+def pop_with_args(array)
+  k = array.pop(2)
+  return k  
+end
+
+def using_shift(array)
+  k = array.shift
+  return k 
+end
+
+def shift_with_args(array)
+  k = array.shift(2)
+  return k 
+end
+
+#shift_with_args(["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"])
+  
+
+
+
+  
+  
