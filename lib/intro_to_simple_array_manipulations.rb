927a98852ff2b7332c86a6ab0258d30d86cc8ad1
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-29 03:40:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,12 @@ def using_push(array, string)
   updated_array = array.push(string)
 end
 
+def using_unshift(array, string)
+  updated_array = array.unshift(string)
+end
+
+def using_pop(array)
+  
 
 
 
