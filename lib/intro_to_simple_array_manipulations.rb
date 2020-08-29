94aa4c84ee574316ebb06be8e397d045ad63e68c
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-29 03:28:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,5 @@
 def using_push(array, string)
   updated_array = array.push(string)
-  puts updated_array.last
 end
 
 using_push(colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"], next_color = "violet" )
